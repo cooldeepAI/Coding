ac1c3033b025695dbb7037c4acd67447d07bb745
Shuangzhen <dreamre21@gmail.com>
2019-09-23 15:14:02 +0800

    Add leetcoed#4

---
+++ Python/Leetcode/array/4_RemoveDuplicatesfromSortedArray.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-09-23 15:10
+# @Author  : Yang Shuangzhen
+# @File    : 4_RemoveDuplicatesfromSortedArray.py
+
+class Solution():
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ Python/Leetcode/array/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-09-23 15:12
+# @Author  : Yang Shuangzhen
+# @File    : __init__.py.py
