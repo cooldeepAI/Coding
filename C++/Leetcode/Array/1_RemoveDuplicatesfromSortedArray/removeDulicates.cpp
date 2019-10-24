e015e4b96e4e7e052cc174912e2090f3db1a0cea
Shuangzhen <dreamre21@gmail.com>
2019-10-24 19:59:45 +0800

    Add Aarray practice and clification

---
+++ C++/Leetcode/Array/1_RemoveDuplicatesfromSortedArray/removeDulicates.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Yang Shuangzhen on 2019/10/24.
+//
+#include <iostream>
+using namespace std;
+
+#define LENGTH 3
+
+class Solution{
+public:
+    int removeDuplicates(int A[], int n){
+
+        return 0;
+    }
+};
+
+int main(){
+    int A[LENGTH] = {1, 1, 2};
+    Solution sol;
+    int result = sol.removeDuplicates(A, LENGTH);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
